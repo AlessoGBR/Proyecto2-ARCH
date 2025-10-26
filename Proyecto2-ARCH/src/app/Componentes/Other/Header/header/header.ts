5573ecf836ad2c68f579140fa7ec723938b2c761
Diego <alessogoldodiego@gmail.com>
2025-10-26 11:46:15 -0600

    Cambios para netlify

--- Proyecto2-ARCH/src/app/Componentes/Other/Header/header/header.ts
+++ Proyecto2-ARCH/src/app/Componentes/Other/Header/header/header.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Auth } from '../../../../Services/auth';
 import Swal from 'sweetalert2';
-import { ProductosService } from '../../../../Services/Productos/productos-service';
 import { Router } from '@angular/router';
 
 @Component({
